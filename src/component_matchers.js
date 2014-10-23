7dafdd3178cee4aaf5bc9eb7596bbf3d7e7fbcb0
Sasha Koss <koss@nocorp.me>
2014-10-23 23:09:01 +0400

    Hello cruel world!

---
+++ src/component_matchers.js
@@ -0,0 +1,414 @@
+var React = require('react');
+var cycle = require('cycle');
+var RewireTestHelpers = require('rewire-test-helpers');
+
+/**
+ * Set of Chai matchers aimed to simplify testing of React components.
+ */
+var componentMatchers = function(_chai, utils) {
+  var Assertion = chai.Assertion;
+  var flag = utils.flag;
+
+  /**
+   * Renders component and return $el
+   *
+   * @returns {jQuery object}
+   */
+  _renderComponent = function(context) {
+    var obj = context._obj;
+    var $component = obj();
+    if (obj.andFunction) {
+      obj.andFunction($component);
+    }
+
+    // We are returning not $component it self but content of playground
+    // because in the "and" function state may be changed and old
+    // element can be changed.
+    return $('#playground >');
+  };
+
+  /**
+   * Chain property, sets render flag to true.
+   */
+  Assertion.addProperty('render', function() {
+    var obj = this._obj;
+    flag(this, 'render', true);
+  });
+
+  /**
+   * Matcher that ensures that component renders given text.
+   *
+   * @example
+   * expect(render.with(null, 'Click me!')).to.render.text('Click me!');
+   */
+  Assertion.addMethod('text', function(text) {
+    if (!flag(this, 'render')) return;
+
+    var $component = _renderComponent(this);
+
+    this.assert(
+      $component.is(":contains('" + text + "')"),
+      'expected component to render text "' + text + '"',
+      'expected component to not render text "' + text + '"'
+    );
+  });
+
+  /**
+   * @param {Object} context chai context
+   * @param {jQuery object} $el
+   * @param {string} selector
+   */
+  var _expectToContainElement = function(context, $el, selector) {
+    context.assert(
+      $el.length >= 1,
+      'expected component to contain el "' + selector + '"',
+      'expected component to not contain el "' + selector + '"'
+    );
+  };
+
+  /**
+   * @param {Object} context chai context
+   * @param {jQuery object} $el
+   * @param {string} attrName
+   * @param {string} value
+   */
+  var _expectToHaveAttr = function(context, $el, attrName, value) {
+    if (value && value.constructor && value.constructor == RegExp) {
+      var attr = $el.attr(attrName);
+      context.assert(
+        value.test(attr),
+        'expected el to have attr "' + attrName + '" matches "' + value + '"',
+        'expected el to not have attr "' + attrName + '" matches "' + value + '"'
+      );
+
+    } else {
+      context.assert(
+        $el.is("[" + attrName + "='" + value + "']"),
+        'expected el to have attr "' + attrName + '" equal to "' + value + '"',
+        'expected el to not have attr "' + attrName + '" equal to "' + value + '"'
+      );
+    }
+  };
+
+  /**
+   * @param {Object} context chai context
+   * @param {jQuery object} $el
+   * @param {string} attrs
+   */
+  var _expectToHaveAttrs = function(context, $el, attrs) {
+    Object.keys(attrs).forEach(function(attrName) {
+      var value = attrs[attrName];
+      _expectToHaveAttr(context, $el, attrName, value);
+    });
+  };
+
+  /**
+   * @param {Object} context chai context
+   * @param {jQuery object} $el
+   * @param {string} propName
+   * @param {string} value
+   */
+  var _expectToHaveProp = function(context, $el, propName, value) {
+    context.assert(
+      $el.prop(propName) == value,
+      'expected el to have prop "' + propName + '" equal to "' + value + '"',
+      'expected el to not have prop "' + propName + '" equal to "' + value + '"'
+    );
+  };
+
+  /**
+   * @param {Object} context chai context
+   * @param {Object} $el
+   * @param {string} props
+   */
+  var _expectToHaveProps = function(context, $el, props) {
+    Object.keys(props).forEach(function(propName) {
+      var value = props[propName];
+      _expectToHaveProp(context, $el, propName, value);
+    });
+  };
+
+  /**
+    * @param {object} context chai context
+    * @param {jQuery object} $el
+    * @param {string} selector
+   */
+  var _expectToMatchSelector = function(context, $el, selector) {
+    context.assert(
+      $el.is(selector),
+      'expected el to match "' + selector + '"',
+      'expected el to not match "' + selector + '"'
+    );
+  };
+
+  /**
+    * @param {object} context chai context
+    * @param {jQuery object} $el
+    * @param {string} expectedValue
+   */
+  var _expectToHaveValue = function(context, $el, expectedValue) {
+    var actualValue = $el.val();
+    context.assert(
+      expectedValue == actualValue,
+      'expected el to have value "' + expectedValue + '" but have "' + actualValue + '"',
+      'expected el to not have value "' + expectedValue + '" but have "' + actualValue + '"'
+    );
+  };
+
+  /**
+    * @param {Object} context chai context
+    * @param {jQuery object} $el
+    * @param {string} text
+   */
+  var _expectToContainText = function(context, $el, text) {
+    context.assert(
+      $el.is(":contains('" + text + "')"),
+      'expected component to render text "' + text + '"',
+      'expected component to not render text "' + text + '"'
+    );
+  };
+
+  /**
+   * Matcher that ensures that component's el/nested el has given
+   * class/text/attr.
+   * @param {Object} expectations object
+   *
+   * @example
+   * expect(render.with({ color: red }, 'Click me!')).to.render.el({
+   *   is: '.is-red',
+   *   href: '#',
+   *   text: 'Click me!'
+   * });
+   *
+   * @example
+   * expect(render.with({ color: red }, 'Click me!')).to.render.el({
+   *   find: 'a',
+   *   is: '.is-red',
+   *   href: '#',
+   *   text: 'Click me!'
+   * });
+   */
+  Assertion.addChainableMethod('el', function(expectations) {
+    if (!flag(this, 'render')) return;
+
+    var $component = _renderComponent(this);
+
+    var attrs = Object.reject(
+      expectations, 'find', 'is', 'text', 'value', 'props'
+    );
+
+    var $el;
+    if (expectations.find) {
+      var selector = expectations.find;
+      $el = $component.find(selector);
+      _expectToContainElement(this, $el, selector);
+
+    } else {
+      $el = $component;
+    }
+
+    if (expectations.is) _expectToMatchSelector(this, $el, expectations.is);
+    if (expectations.text) _expectToContainText(this, $el, expectations.text);
+    if (expectations.value) _expectToHaveValue(this, $el, expectations.value);
+    if (expectations.props) _expectToHaveProps(this, $el, expectations.props);
+
+    _expectToHaveAttrs(this, $el, attrs);
+
+  }, function() {
+    if (!flag(this, 'render')) return;
+
+    var $component = _renderComponent(this);
+
+    flag(this, '$component', $component);
+  });
+
+  /**
+    * Matcher that ensures that component have specefied class.
+    * @param {string} className
+   */
+  Assertion.addMethod('withClass', function(className) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToMatchSelector(this, $component, '.' + className);
+  });
+
+  /**
+    * Matcher that ensures that component have expected value.
+    * @param {string} className
+   */
+  Assertion.addMethod('withValue', function(value) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToHaveValue(this, $component, value);
+  });
+
+  /**
+   * Matcher that ensures that component match selector.
+   * @param {string} selector
+   */
+  Assertion.addMethod('matches', function(selector) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToMatchSelector(this, $component, selector);
+  });
+
+  /**
+   * Matcher that ensures that component's attr has specific value.
+   * @param {string} attrName
+   * @param {string} value
+   */
+  Assertion.addMethod('withAttr', function(attrName, value) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToHaveAttr(this, $component, attrName, value);
+  });
+
+  /**
+   * Matcher that ensures that component have specific attrs.
+   * @param {Object} attrs
+   */
+  Assertion.addMethod('withAttrs', function(attrs) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToHaveAttrs(this, $component, attrs);
+  });
+
+  /**
+   * Matcher that ensures that component's prop has specific value.
+   * @param {string} propName
+   * @param {string} value
+   */
+  Assertion.addMethod('withProp', function(propName, value) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToHaveProp(this, $component, propName, value);
+  });
+
+  /**
+   * Matcher that ensures that component have specific props.
+   * @param {Object} props
+   */
+  Assertion.addMethod('withProps', function(props) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    _expectToHaveProps(this, $component, props);
+  });
+
+
+  /**
+   * Matcher that ensures that component have nested el matches passed selector.
+   * @param {string} selector
+   */
+  Assertion.addMethod('contains', function(selector) {
+    var $component = flag(this, '$component');
+    if (!$component) return;
+    var $el = $component.find(selector);
+    _expectToContainElement(this, $el, selector);
+  });
+
+  /**
+   * @param {Object} context chai context
+   * @param {string} componentName
+   * @param {SinonSpy} spy
+   */
+  var _expectComponentToRender = function(context, componentName, spy) {
+    context.assert(
+      spy.called,
+      'expected component to render ' + componentName,
+      'expected component to not render ' + componentName
+    )
+  }
+
+  /**
+   * @param {Object} context chai context
+   * @param {string} componentName
+   * @param {SinonSpy} spy
+   * @param {*[]} args
+   */
+  var _expectComponentToRenderWith =
+    function(context, componentName, spy, args) {
+
+    var props = args[0];
+    var children = args[1];
+
+    var errorMessageEnd;
+    if (spy.called) {
+      errorMessageEnd =
+        'but called with ' + JSON.stringify(cycle.decycle(spy.args));
+    } else {
+      errorMessageEnd = 'but never been called';
+    }
+
+    context.assert(
+      spy.calledWith.apply(spy, args),
+      'expected component to render ' + componentName + ' with ' +
+      JSON.stringify(args) + ' ' + errorMessageEnd,
+      'expected component to not render ' + componentName + ' with ' +
+      JSON.stringify(args) + ' ' + errorMessageEnd
+    );
+  };
+
+  /**
+   * Matcher that ensures that component render another component(s)
+   * @param {ReactComponent} componentName
+   * @param {boolean} single
+   * @param {Object} options
+   */
+  var _expectComponentToRenderComponent = function(componentName, single, options) {
+    if (!flag(this, 'render')) return;
+    options = options || {};
+    var negate = flag(this, 'negate');
+
+    var Component = this._obj.Component;
+
+    var DummyComponent = React.createClass({
+      render: function() {
+        return this.transferPropsTo(React.DOM.div(null, this.props.children));
+      }
+    });
+
+    var spy = sinon.spy(DummyComponent);
+    var $component;
+
+    var overrides = {};
+    overrides[componentName] = spy;
+    RewireTestHelpers.rewired(Component, overrides, ()=> {
+      $component = _renderComponent(this);
+    });
+
+    if (!negate || !options.with) {
+      _expectComponentToRender(this, componentName, spy);
+    }
+
+    if (options.with) {
+      if (single) {
+        _expectComponentToRenderWith(this, componentName, spy, options.with);
+      } else {
+        options.with.forEach(
+          _expectComponentToRenderWith.bind(null, this, componentName, spy)
+        );
+      }
+    }
+  };
+
+  /**
+   * Matcher that ensures that component render another component
+   * @param {ReactComponent} componentName
+   * @param {Object} options
+   */
+  Assertion.addMethod('component', function(componentName, options) {
+    _expectComponentToRenderComponent.call(this, componentName, true, options);
+  });
+
+  /**
+   * Matcher that ensures that component render another component
+   * @param {ReactComponent} componentName
+   * @param {Object} options
+   */
+  Assertion.addMethod('components', function(componentName, options) {
+    _expectComponentToRenderComponent.call(this, componentName, false, options);
+  });
+};
+
+module.exports = componentMatchers;
+
---
+++ src/describe_component.js
@@ -0,0 +1,76 @@
+var React = require('react/addons');
+var cycle = require('cycle');
+var PlaygroundTestHelpers = require('playground-test-helpers');
+
+/**
+ * Helper that simplifies describing of component. It takes component name,
+ * prepares playground for tests and pass render to context function.
+ *
+ * @param {React component} Component
+ * @param {function} contextFn
+ */
+var describeComponent = function(Component, contextFn) {
+
+  var componentName = Component.displayName;
+
+  var render = function(props, children) {
+    var component = Component(props, children);
+    return PlaygroundTestHelpers.$render(component, React);
+  };
+
+  var setComponentDataTo = function(fn) {
+    fn.Component = Component;
+    fn.componentName = componentName;
+  };
+
+  var setAndDataTo = function(fn) {
+    fn.and = (andFunction)=> {
+      fn.andFunction = andFunction;
+      return fn;
+    }
+  };
+
+  render.with = function(props, children) {
+    var bindedRender = render.bind(PlaygroundTestHelpers, props, children);
+    setComponentDataTo(bindedRender);
+    setAndDataTo(bindedRender);
+    return bindedRender;
+  };
+
+  setComponentDataTo(render);
+  setAndDataTo(render);
+
+  var helpers = {
+    mockDepsFilter: function() {
+      var DummyWhatever = React.createClass({
+        render: function() {
+          var propsJSON = JSON.stringify(cycle.decycle(this.props));
+          var props = { className: this.props.className, 'data-props': propsJSON };
+          var children = this.props.children;
+
+          return React.DOM.div(props, children);
+        }
+      });
+
+      var deps = {};
+      Array.create(arguments).forEach(function(componentName) {
+        deps[componentName] = DummyWhatever;
+      });
+      injectDependenciesFilter(Component, deps);
+    },
+
+    simulate: React.addons.TestUtils.Simulate,
+
+    el: function() {
+      return document.getElementById('playground').firstChild;
+    }
+  };
+
+  describe(componentName, function() {
+    beforeEach(PlaygroundTestHelpers.prepare);
+    contextFn(render, helpers);
+  });
+};
+
+module.exports = describeComponent;
+
